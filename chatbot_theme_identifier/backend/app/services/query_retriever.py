462a0722b33c626b586464cd86d567153976b2b2
palakbansal8810 <140965358+palakbansal8810@users.noreply.github.com>
2025-06-13 13:13:35 +0530

    data update

--- chatbot_theme_identifier/backend/app/services/query_retriever.py
+++ chatbot_theme_identifier/backend/app/services/query_retriever.py
@@ -13,7 +13,7 @@ os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 llm = ChatGroq(model_name="llama3-8b-8192")
 
 
-def ask_question(query, memory, retriever, llm, top_k=5):
+def ask_question(query, memory, retriever, llm, top_k=8):
     docs = retriever.vectorstore.similarity_search(query, k=top_k)
 
     sources = []
